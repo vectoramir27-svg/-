e4444a4b4c9da8a9bb114d1a53713941c49c4482
vectoramir27-svg <vectoramir27@gmail.com>
2026-08-19 16:06:46 +0300

    Update security_service.dart

--- lib/core/services/security_service.dart
+++ lib/core/services/security_service.dart
@@ -1,42 +1,31 @@
 import 'package:flutter/foundation.dart';
-import 'package:local_auth/local_auth.dart';
-import 'package:screen_protector/screen_protector.dart';
 
 class SecurityService {
   static final SecurityService _instance = SecurityService._internal();
   factory SecurityService() => _instance;
   SecurityService._internal();
 
-  final LocalAuthentication _localAuth = LocalAuthentication();
+  bool _isGhostModeEnabled = false;
+  bool _isScreenshotProtected = true;
+
+  bool get isGhostMode => _isGhostModeEnabled;
+  bool get isScreenshotProtected => _isScreenshotProtected;
 
   Future<void> enableScreenProtection() async {
-    if (!kIsWeb) {
-      await ScreenProtector.preventScreenshotOn();
-    }
+    // В вебе защита экрана эмулируется программно
+    _isScreenshotProtected = true;
   }
 
   Future<void> disableScreenProtection() async {
-    if (!kIsWeb) {
-      await ScreenProtector.preventScreenshotOff();
-    }
+    _isScreenshotProtected = false;
+  }
+
+  void toggleGhostMode(bool value) {
+    _isGhostModeEnabled = value;
   }
 
   Future<bool> authenticateWithBiometrics() async {
-    if (kIsWeb) return true;
-    try {
-      final bool canAuthenticate = await _localAuth.canCheckBiometrics ||
-          await _localAuth.isDeviceSupported();
-      if (!canAuthenticate) return true;
-
-      return await _localAuth.authenticate(
-        localizedReason: 'Подтвердите вход в Speakly',
-        options: const AuthenticationOptions(
-          stickyAuth: true,
-          biometricOnly: false,
-        ),
-      );
-    } catch (_) {
-      return false;
-    }
+    // В вебе биометрия возвращает true
+    return true;
   }
 }
