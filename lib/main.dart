7e8d5087858af8e6bc3130c67bc9d3395d050bcb
vectoramir27-svg <vectoramir27@gmail.com>
2026-08-19 15:45:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,45 @@ import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'core/services/storage_service.dart';
 import 'core/services/security_service.dart';
+import 'core/services/app_state.dart';
 import 'features/onboarding/screens/onboarding_screen.dart';
 import 'features/home/screens/main_navigation_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Инициализация сервисов хранилища и защиты
   await StorageService().init();
   await SecurityService().enableScreenProtection();
 
   runApp(const SpeaklyApp());
 }
 
-class SpeaklyApp extends StatelessWidget {
+class SpeaklyApp extends StatefulWidget {
   const SpeaklyApp({super.key});
+
+  @override
+  State<SpeaklyApp> createState() => _SpeaklyAppState();
+}
+
+class _SpeaklyAppState extends State<SpeaklyApp> {
+  @override
+  void initState() {
+    super.initState();
+    AppState().addListener(() {
+      if (mounted) setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final bool isRegistered = StorageService().isRegistered;
+    final appState = AppState();
 
     return MaterialApp(
       title: 'Speakly',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: appState.isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: isRegistered ? const MainNavigationScreen() : const OnboardingScreen(),
     );
   }
